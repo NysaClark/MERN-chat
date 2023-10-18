d61b676fb70192e525e76f9e330af5ebfeff19b3
Nysa <naominysa@gmail.com>
2023-10-18 15:58:16 -0700

    started socket.io

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 
+const initSocket = require("./socket/index")
+
 const app = express();
 const PORT = 4000;
 
@@ -12,13 +14,6 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 
 const authMiddleware = require("./middleware/authMiddleware");
-
-// import init socket
-// const socketIO = require("socket.io")(http, {
-//   cors: {
-//     origin: "http://localhost:5173",
-//   },
-// });
 
 const corsOptions = {
   //connect to frontend
@@ -36,90 +31,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 //users
 
-// let users = [];
 
-// // Join user to chat
-// const newUser = (id, username) => {
-//   const user = { id, username };
-
-//   users.push(user);
-
-//   return user;
-// }
-
-// // Get current user
-// const getActiveUser = (id) => {
-//   return users.find((user) => user.id === id);
-// }
-
-// // User leaves chat
-// const exitRoom = (id) => {
-//   const index = users.findIndex((user) => user.id === id);
-
-//   if (index !== -1) {
-//     return users.splice(index, 1)[0];
-//   }
-// }
-
-// // Get room users
-// const getRoomUsers = () => {
-//   return users;
-// }
-
-// // Listen for incoming connections from clients
-// socketIO.on("connection", (socket) => {
-//   socket.on("joinRoom", ({ username }) => {
-//     const user = newUser(socket.id, username);
-
-//     socket.join("Room 1");
-
-//     // General welcome
-//     socket.emit("message", {
-//       username: "Chat",
-//       text: "Messages are limited to this room!",
-//     });
-
-//     // Broadcast everytime users connects
-//     socket.broadcast.to("Room 1").emit("message", {
-//       username: "Chat",
-//       text: `${user.username} has joined the room`,
-//     });
-
-//     // Current active users and room name
-//     socketIO.to("Room 1").emit("roomUsers", {
-//       users: getRoomUsers(),
-//     });
-
-//     // Listen for client message
-//     socket.on("chatMessage", ({msg}) => {
-//       console.log(msg)
-//       const user = getActiveUser(socket.id);
-//       socketIO.to("Room 1").emit("message", { username: user.username, text: msg, socketID: socket.id});
-//     });
-
-//     socket.on("userLeft", () => {
-//       socket.disconnect();
-//     })
-//   });
-
-//   // Runs when client disconnects
-//   socket.on("disconnect", () => {
-//     const user = exitRoom(socket.id);
-
-//     if (user) {
-//       socketIO.to("Room 1").emit("message", {
-//         username: "Chat",
-//         text: `${user.username} has left the room`,
-//       });
-
-//       // Current active users and room name
-//       socketIO.to("Room 1").emit("roomUsers", {
-//         room: "Room 1",
-//         users: getRoomUsers(),
-//       });
-//     }
-//   });
-// });
 
 mongoose
   .connect(process.env.MONGO_URI)
@@ -133,3 +45,4 @@ const server = app.listen(PORT, () => {
 });
 
 // init socket w/ server & corsOptions
+initSocket(server, corsOptions)
---
+++ backend/socket/index.js
@@ -0,0 +1,102 @@
+// const socketIO = require("socket.io")(http, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//   },
+// });
+
+// let users = [];
+
+// // Join user to chat
+// const newUser = (id, username) => {
+//   const user = { id, username };
+
+//   users.push(user);
+
+//   return user;
+// }
+
+// // Get current user
+// const getActiveUser = (id) => {
+//   return users.find((user) => user.id === id);
+// }
+
+// // User leaves chat
+// const exitRoom = (id) => {
+//   const index = users.findIndex((user) => user.id === id);
+
+//   if (index !== -1) {
+//     return users.splice(index, 1)[0];
+//   }
+// }
+
+// // Get room users
+// const getRoomUsers = () => {
+//   return users;
+// }
+
+// // Listen for incoming connections from clients
+// socketIO.on("connection", (socket) => {
+//   socket.on("joinRoom", ({ username }) => {
+//     const user = newUser(socket.id, username);
+
+//     socket.join("Room 1");
+
+//     // General welcome
+//     socket.emit("message", {
+//       username: "Chat",
+//       text: "Messages are limited to this room!",
+//     });
+
+//     // Broadcast everytime users connects
+//     socket.broadcast.to("Room 1").emit("message", {
+//       username: "Chat",
+//       text: `${user.username} has joined the room`,
+//     });
+
+//     // Current active users and room name
+//     socketIO.to("Room 1").emit("roomUsers", {
+//       users: getRoomUsers(),
+//     });
+
+//     // Listen for client message
+//     socket.on("chatMessage", ({msg}) => {
+//       console.log(msg)
+//       const user = getActiveUser(socket.id);
+//       socketIO.to("Room 1").emit("message", { username: user.username, text: msg, socketID: socket.id});
+//     });
+
+//     socket.on("userLeft", () => {
+//       socket.disconnect();
+//     })
+//   });
+
+//   // Runs when client disconnects
+//   socket.on("disconnect", () => {
+//     const user = exitRoom(socket.id);
+
+//     if (user) {
+//       socketIO.to("Room 1").emit("message", {
+//         username: "Chat",
+//         text: `${user.username} has left the room`,
+//       });
+
+//       // Current active users and room name
+//       socketIO.to("Room 1").emit("roomUsers", {
+//         room: "Room 1",
+//         users: getRoomUsers(),
+//       });
+//     }
+//   });
+// });
+
+const initSocket = (server, corsOptions) => {
+  const io = require("socket.io")(server, { cors: corsOptions });
+
+  let onlineUsers = [];
+
+  io.on("connection", socket => {
+    
+  })
+};
+
+module.exports = initSocket;
