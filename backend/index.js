6c3e077d7cbadc6e3184ef4e7f509f5ea235f6b8
Nysa <naominysa@gmail.com>
2023-11-06 16:40:08 -0700

    cors origin change

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const authMiddleware = require("./middleware/authMiddleware");
 const corsOptions = {
   //connect to frontend
   // origin: ["https://mern-chat-l99i.onrender.com", "http://localhost:5173"],
-  origin: "https://mern-chat-l99i.onrender.com",
+  origin: "https://mern-chat-l99i.onrender.com/login",
   allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
   credentials: true,
   methods: ['GET','POST'],
