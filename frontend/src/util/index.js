d07f6492d982df2f1ca0eb8225550130bc8aeb29
Nysa <naominysa@gmail.com>
2023-11-28 13:08:35 -0700

    links :(

--- frontend/src/util/index.js
+++ frontend/src/util/index.js
@@ -1,2 +1,2 @@
-export const baseURL = `http://localhost:4000`
-// export const baseURL = "https://mern-chat-app-b96k.onrender.com"
+// export const baseURL = `http://localhost:4000`
+export const baseURL = "https://mern-chat-app-b96k.onrender.com"
